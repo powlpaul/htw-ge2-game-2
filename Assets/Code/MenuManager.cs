b11bfe53d12f4f3e549f27d2421da7a8b40ed9eb
LennoxCode <leonard.valentin1999@gmail.com>
2022-01-25 11:21:09 +0100

    added scaffold script for menu management

---
+++ Assets/Code/MenuManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private Text healthDisplay;
+    [SerializeField] private Text moneyDisplay;
+    [SerializeField] private Text RoundDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setup()
+    {
+
+    }
+    public void updateHealth(int newHealth)
+    {
+
+    }
+    public void updateMoney(int newMoney)
+    {
+
+    }
+    public void nextRound()
+    {
+
+    }
+}
