5920706fb081f6589b394b3a1bbfff1dcab43188
LennoxCode <leonard.valentin1999@gmail.com>
2022-01-25 14:19:23 +0100

    added info for how much money the player has

--- Assets/Code/PlayerStats.cs
+++ Assets/Code/PlayerStats.cs
@@ -8,17 +8,20 @@ public class PlayerStats : MonoBehaviour {
 
 	public static int Lives;
 	[SerializeField] int startLives = 20;
-
+	public static MenuManager menu;
 	public static int Rounds;
 
 	void Start ()
 	{
-		MenuManager menu = GameObject.Find("MenuCanvas").GetComponent<MenuManager>();
+		menu = GameObject.Find("MenuCanvas").GetComponent<MenuManager>();
 		menu.UpdateHealth(startLives);
 		menu.UpdateMoney(startMoney);
 		Money = startMoney;
 		Lives = startLives;
 		Rounds = 0;
 	}
-
+    private void Update()
+    {
+		menu.UpdateMoney(Money);
+    }
 }
