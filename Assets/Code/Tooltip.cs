8d185a80ff440e43d9b328fd36ab26049c89ccd3
LennoxCode <leonard.valentin1999@gmail.com>
2022-02-03 20:27:17 +0100

    added delay and pivot to tooltip

--- Assets/Code/Tooltip.cs
+++ Assets/Code/Tooltip.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [ExecuteInEditMode()]
+
+/*this class was coded using the following tutorial:
+ * https://www.youtube.com/watch?v=HXFoUGw7eKk
+ * just like the camera. adding an menu item which is displayed contitionally 
+ * based on mouse position is not that difficult
+ * but doing it properly can prove challenging. This tutorial proved 
+ * as a great ressource for exactly that
+ */
 public class Tooltip : MonoBehaviour
 {
     public TextMeshProUGUI headerField;
@@ -12,7 +20,12 @@ public class Tooltip : MonoBehaviour
     public TextMeshProUGUI contentField;
     public LayoutElement LayoutElement;
     public int characterWrapLimit;
+    public RectTransform rectTransform;
 
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
     public void SetText(string content, string header ="")
     {
         if (string.IsNullOrEmpty(header)) headerField.gameObject.SetActive(false);
@@ -28,6 +41,9 @@ public class Tooltip : MonoBehaviour
     private void Update()
     {
         Vector2 position = Input.mousePosition;
+        float pivotX = position.x / Screen.width;
+        float pivotY = position.y / Screen.height;
+        rectTransform.pivot = new Vector2(pivotX, pivotY);
         transform.position = position;
     }
 }
--- Assets/Code/TooltipTrigger.cs
+++ Assets/Code/TooltipTrigger.cs
@@ -4,19 +4,26 @@ using UnityEngine.EventSystems;
 
 class TooltipTrigger : MonoBehaviour, IPointerEnterHandler,  IPointerExitHandler
 {
-    private float TimeOverElement;
+  
+    private bool courseInArea;
     public string header;
     [TextArea]
     public string content;
     public void OnPointerEnter(PointerEventData eventData)
     {
-  
-        TooltipSystem.Show(content, header);
+        courseInArea = true;
+        Waiter.Wait(0.5f, () => {
+            if(courseInArea)TooltipSystem.Show(content, header);
+        });
+        
+      
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        courseInArea = false;
         TooltipSystem.Hide();
+
     }
 
 
