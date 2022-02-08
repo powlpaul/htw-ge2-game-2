3fa1d96c779896f2585d34403b941b9694a231fa
pandanmochi <to.un.thi@gmail.com>
2022-02-08 01:11:19 +0100

    finish dialogue scene

--- Assets/Code/DialogueManager.cs
+++ Assets/Code/DialogueManager.cs
@@ -6,10 +6,11 @@ using UnityEngine.UI;
 public class DialogueManager : MonoBehaviour
 {
     public Text nameText;
-
     public Text dialogueText;
-
     public Animator animator;
+    public Animator ballAnimator;
+    public Animator blueBirbAnimator;
+    public Animator brownBirbAnimator;
 
     public Queue<string> sentences;
 
@@ -22,8 +23,11 @@ public class DialogueManager : MonoBehaviour
     public void StartDialogue(Dialogue dialogue)
     {
         animator.SetBool("IsOpen", true);
-        Debug.Log("Starting conversation with" + dialogue.name);
-        nameText.text = dialogue.name;
+        ballAnimator.SetBool("ballHasEntered", true);
+        blueBirbAnimator.SetBool("blueEntered", true);
+        brownBirbAnimator.SetBool("brownEntered", true);
+        
+        
         sentences.Clear();
 
         foreach (string sentence in dialogue.sentences)
@@ -43,11 +47,25 @@ public class DialogueManager : MonoBehaviour
 
         string sentence = sentences.Dequeue();
         dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+
+    }
+
+    IEnumerator TypeSentence(string sentence) {
+      dialogueText.text = "";
+      foreach(char letter in sentence.ToCharArray()) {
+        dialogueText.text += letter;
+        yield return null;
+      }
     }
 
     void EndDialogue()
     {
         Debug.Log("End of conversation");
         animator.SetBool("IsOpen", false);
+        ballAnimator.SetBool("ballHasEntered", false);
+        blueBirbAnimator.SetBool("blueEntered", false);
+        brownBirbAnimator.SetBool("brownEntered", false);
     }
 }
